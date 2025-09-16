9291ae369ddb3045f9a6e07603bf5a2c32afc1d3
KacperTetela <kacpertetela1@gmail.com>
2025-09-16 02:17:29 +0200

    spring default port update

---
+++ src/main/java/config/OpenApiConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    servers = {
+      @Server(url = "/", description = "")
+    })
+public class OpenApiConfig {}
